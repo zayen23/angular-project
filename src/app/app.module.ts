3543d06b782407197c982a434f271e029499c78c
Zayen Dev <mohamedzayen053@gmail.com>
2025-11-26 18:32:08 +0100

    Mise à jour du projet Angular avec tous les composants et corrections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { ListeSuggestionsComponent } from './core/liste-suggestions/liste-suggestions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListeSuggestionsComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/models/suggestion.ts
@@ -0,0 +1,12 @@
+export interface Suggestion {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  date: Date;
+  status: string;
+  likes?: number;
+}
+
+
+
